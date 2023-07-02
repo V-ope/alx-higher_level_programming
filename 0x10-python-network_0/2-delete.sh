7947567d454ba81a6f7c40cd61442df65f761f73
V-ope <opeyemivictor04@gmail.com>
2023-07-02 16:45:10 -0700

    Update

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This is a bash script n that sends a DELETE request to an URL with curl and display the body of the response
 
-curl -sX DELETE "$vip"
+curl -sX DELETE "$1"
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This is a bash script that sends a GET request to the URL using curl, and display the body of the response
 
-curl -sH "X-School-User-Id: 98" "$1"
+curl -s -x GET -H "X-School-User-Id: 98" "$1"
