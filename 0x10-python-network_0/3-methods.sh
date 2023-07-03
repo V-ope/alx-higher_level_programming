da8af93555db6cc4ed9e1d95d3e0512da58502ca
V-ope <opeyemivictor04@gmail.com>
2023-07-02 17:24:03 -0700

    Update

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # This is a bash script that displays all HTTP methods server will accept
-
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
+curl -sI "$1" | grep "Allow" | awk '{print substr($0, index($0,$2))}'
